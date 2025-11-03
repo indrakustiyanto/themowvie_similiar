b2ec9a24598a822f963b5c017e2026f7befabb93
indra saputro <indrasaputro65@gmail.com>
2025-11-03 19:25:08 +0700

    init app2

--- app.py
+++ app.py
@@ -36,9 +36,8 @@ def recomendationsMovieTv(type, movieId):
 def home():
     return "Mowvie Recomendation API is running."
 
-
-if __name__ == '__main__':
-    app.run()
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
